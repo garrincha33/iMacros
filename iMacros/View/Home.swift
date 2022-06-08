7a690ca081a4a89873edf922ce43d37fb50aa151
Richard Price <richyprice77@gmail.com>
2022-06-08 20:28:22 +0100

    created data model and start UI

---
+++ iMacros/View/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  iMacros
+//
+//  Created by Richard Price on 08/06/2022.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
